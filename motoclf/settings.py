ebc7aa821cf9854cc2ad07feb2340f3826960658
RicardoMV <ricardom.ipn@gmail.com>
2020-11-05 16:51:25 -0600

    everything goes ok, now debug=False

--- motoclf/settings.py
+++ motoclf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
