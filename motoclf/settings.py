9b1b82bbea367dbd3212212f5b600e3e2d97cce6
RicardoMV <ricardom.ipn@gmail.com>
2020-11-05 14:13:17 -0600

    changes for debugging online

--- motoclf/settings.py
+++ motoclf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
