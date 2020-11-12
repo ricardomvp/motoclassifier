8fb9a1dfc1aaabe0c919df32b8424ac1762d43c1
RicardoMV <ricardom.ipn@gmail.com>
2020-11-12 15:04:08 -0600

    Enable debug online by Heroku & fix typo in pagination

--- motoclf/settings.py
+++ motoclf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
